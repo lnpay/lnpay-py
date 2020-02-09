89805e4022111f1a5810402307c10a267ad7aede
AnthonyRonning <anthonyronning@gmail.com>
2020-02-09 17:04:40 -0600

    Adding a test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,9 +1,21 @@
 import unittest
+import lnpay_py
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, True)
+class TestInit(unittest.TestCase):
+    def test_initialization(self):
+        key = "123"
+        default_wak = "345"
+        endpoint = "https://google.com"
+        params = {
+            "endpoint_url": endpoint
+        }
+
+        lnpay_py.initialize(key, default_wak, params)
+
+        self.assertEqual(key, lnpay_py.__PUBLIC_API_KEY__)
+        self.assertEqual(default_wak, lnpay_py.__DEFAULT_WAK__)
+        self.assertEqual(endpoint, lnpay_py.__ENDPOINT_URL__)
 
 
 if __name__ == '__main__':
